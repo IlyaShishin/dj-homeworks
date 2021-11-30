f8508b5bd84e46b2cb9108ec67fdb01c67180b34
Ilya Shishin <Shishinilya030996@mail.ru>
2021-11-30 09:11:49 +0400

    HW 3.1 drf-intro Initial Commit

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from measurement.views import SensorCreateAndView, MeasurementCreate, SensorUpdateAndSensorMeasurementView
+
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', SensorCreateAndView.as_view()),
+    path('sensors/<pk>/', SensorUpdateAndSensorMeasurementView.as_view()),
+    path('measurements/', MeasurementCreate.as_view()),
 ]
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,36 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import ListCreateAPIView, CreateAPIView, RetrieveUpdateAPIView, RetrieveAPIView
+from rest_framework.response import Response
+
+from .models import Sensor, Measurement
+from .serializers import SensorSerializer, SensorDetailSerializer
+
+
+class SensorCreateAndView(ListCreateAPIView):
+    queryset = Sensor.objects.all
+    serializer_class = SensorSerializer(queryset, many=True)
+
+    def post(self, request, *args, **kwargs):
+        return Response({'status': 'OK'})
+
+
+class SensorUpdateAndSensorMeasurementView(RetrieveUpdateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = SensorDetailSerializer
+
+    def patch(self, request, *args, **kwargs):
+        return Response({'status': 'OK'})
+
+
+class MeasurementCreate(CreateAPIView):
+    def post(self, request, *args, **kwargs):
+        return Response({'status': 'OK'})
+
+
+
+
+
+
+
+
